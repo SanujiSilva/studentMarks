67bde8e548463d2e494281d58443d6694368b6f2
Sanuji Silva <155705901+SanujiSilva@users.noreply.github.com>
2024-11-28 10:13:54 +0530

    Add files via upload

---
+++ FirstApp/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//using System.Data.SqlClient;
+
+
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+
+
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        //static string connectionString = "Server=DESKTOP-S4OLS6R\\SQLEXPRESS;Persist Security Info=False;initial catalog=StudentMarksDB; Encrypt=False; User Id=; Password=;TrustServerCertificate=True;";
+        static string connectionString = "Data Source=DESKTOP-S4OLS6R\\SQLEXPRESS;Initial Catalog=StudentMarksDB;Integrated Security=True;Trust Server Certificate=True";
+
+        static void Main(string[] args)
+        {
+
+            decimal[] marks = new decimal[10];
+
+            decimal total = 0;
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+
+                Console.Write("Enter your " + (i + 1) + " Mark: ");
+                marks[i] = decimal.Parse(Console.ReadLine());
+
+                total = total + marks[i];
+            }
+            decimal average = total / marks.Length;
+
+            Console.WriteLine();
+            Console.WriteLine("Total = " + total);
+            Console.WriteLine("Average = " + average);
+
+            Console.WriteLine("--------------------------------");
+
+            
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    // Insert into StudentMarks table
+                    string insertStudentQuery = "INSERT INTO StudentMarks (Name, Total, Average) OUTPUT INSERTED.SID VALUES (@Name, @Total, @Average)";
+                    int studentId;
+
+                    using (SqlCommand studentCommand = new SqlCommand(insertStudentQuery, connection, transaction))
+                    {
+                        studentCommand.Parameters.AddWithValue("@Name", name);
+                        studentCommand.Parameters.AddWithValue("@Total", total);
+                        studentCommand.Parameters.AddWithValue("@Average", average);
+
+                        studentId = (int)studentCommand.ExecuteScalar(); 
+                    }
+
+                    string insertMarksQuery = "INSERT INTO Marks (MSID, Mark) VALUES (@MSID, @Mark)";
+                    using (SqlCommand marksCommand = new SqlCommand(insertMarksQuery, connection, transaction))
+                    {
+                        marksCommand.Parameters.AddWithValue("@MSID", studentId);
+                        marksCommand.Parameters.Add("@Mark", System.Data.SqlDbType.Float);
+
+                        foreach (var mark in marks)
+                        {
+                            marksCommand.Parameters["@Mark"].Value = mark;
+                            marksCommand.ExecuteNonQuery();
+                        }
+                    }
+
+                    transaction.Commit(); 
+
+                    Console.WriteLine("Data inserted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback(); 
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+
+
+
+
+                Console.WriteLine();
+                Console.Write("Enter your sudent ID: ");
+                int sid = int.Parse(Console.ReadLine());
+                
+
+                Console.WriteLine();
+
+
+                string queryf = "SELECT SID, Name, Total, Average FROM StudentMarks WHERE SID = @SID";
+
+
+                using (SqlCommand command = new SqlCommand(queryf, connection))
+                {
+                    command.Parameters.AddWithValue("@SID", sid);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+
+                                Console.WriteLine("SID: " + reader["SID"]);
+                                Console.WriteLine("Name: " + reader["Name"]);
+                                Console.WriteLine("Total: " + reader["Total"]);
+                                Console.WriteLine("Average: " + reader["Average"]);
+                                Console.WriteLine();
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No records found for SID = " + sid);
+                        }
+                    }
+
+                }
+
+                string fetchMarksQuery = "SELECT Mark FROM Marks WHERE MSID = @MSID";
+                using (SqlCommand fetchMarksCommand = new SqlCommand(fetchMarksQuery, connection))
+                {
+                    fetchMarksCommand.Parameters.AddWithValue("@MSID", sid);
+                    using (SqlDataReader marksReader = fetchMarksCommand.ExecuteReader())
+                    {
+                        Console.WriteLine("Marks:");
+                        while (marksReader.Read())
+                        {
+                            Console.WriteLine(marksReader["Mark"]);
+                        }
+                    }
+                }
+
+
+
+
+                Console.WriteLine("--------------------------------");
+
+                string queryn = "SELECT Name FROM StudentMarks";
+
+                List<string> allNames = new List<string>();
+
+                using (SqlCommand command = new SqlCommand(queryn, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            allNames.Add(reader["Name"].ToString());
+
+
+                        }
+
+                    }
+
+                    Console.WriteLine("Names:");
+                    Console.WriteLine();
+
+                    foreach (var names in allNames)
+                    {
+                        Console.WriteLine(names);
+                    }
+
+                }
+
+                Console.WriteLine("--------------------------------");
+
+                Student studentManager = new Student(" ", new List<float>(), 0, 0);
+
+
+
+                studentManager.AddStudent(connectionString);
+
+                studentManager.Display();
+
+                
+
+
+                Console.WriteLine("Finished...");
+                Console.ReadKey();
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+              
---
+++ FirstApp/Student.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace FirstApp
+{
+    public class Student
+    {
+        private string name;
+        private List<float> marks; 
+        private float total;
+        private float average;
+
+        public Student(string name, List<float> marks, float total, float average)
+        {
+            this.name = name;
+            this.marks = marks;
+            this.total = total;
+            this.average = average;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public List<float> Marks
+        {
+            get { return marks; }
+            set { marks = value; }
+        }
+
+        public float Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+
+        public float Average
+        {
+            get { return average; }
+            set { average = value; }
+        }
+
+        private List<Student> students = new List<Student>();
+
+        public void AddStudent(string connectionString)
+        {
+            if (students == null)
+                students = new List<Student>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Fetch student details
+                    string queryStudent = "SELECT SID, Name, Total, Average FROM StudentMarks";
+                    using (SqlCommand commandStudent = new SqlCommand(queryStudent, connection))
+                    {
+                        using (SqlDataReader readerStudent = commandStudent.ExecuteReader())
+                        {
+                            while (readerStudent.Read())
+                            {
+                                int sid = Convert.ToInt32(readerStudent["SID"]);
+                                string name = readerStudent["Name"].ToString();
+                                float total = Convert.ToSingle(readerStudent["Total"]);
+                                float average = Convert.ToSingle(readerStudent["Average"]);
+
+                                List<float> marks = GetMarksBySID(connectionString, sid);
+
+                                
+                                students.Add(new Student(name, marks, total, average));
+                            }
+                        }
+                    }
+                }
+
+                //Console.WriteLine("Students successfully added from the database.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private List<float> GetMarksBySID(string connectionString, int sid)
+        {
+            List<float> marks = new List<float>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string queryMarks = "SELECT Mark FROM Marks WHERE MSID = @MSID";
+                using (SqlCommand commandMarks = new SqlCommand(queryMarks, connection))
+                {
+                    commandMarks.Parameters.AddWithValue("@MSID", sid);
+
+                    using (SqlDataReader readerMarks = commandMarks.ExecuteReader())
+                    {
+                        while (readerMarks.Read())
+                        {
+                            marks.Add(Convert.ToSingle(readerMarks["Mark"]));
+                        }
+                    }
+                }
+            }
+
+            return marks;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("List of Students:");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var student in students)
+            {
+                
+                string marksString = string.Join(", ", student.Marks);
+
+                Console.WriteLine($"Name: {student.Name}, Marks: {marksString}, Total: {student.Total}, Average: {student.Average}");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
